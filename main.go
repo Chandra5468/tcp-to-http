38dcab0a269f880db837e50139fe643c528eeb34
Chandra5468 <agirchandra@gmail.com>
2025-10-30 22:17:22 +0530

    at second test

--- main.go
+++ main.go
@@ -1,7 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	f, err := os.Open("./messages.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	buffer := make([]byte, 8)
+	currentLine := ""
+	for {
+		bytesRead, err := f.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				// Handle any remaining data in currentLine at EOF
+				if len(currentLine) > 0 {
+					fmt.Println("Line:", currentLine)
+				}
+				break
+			}
+			fmt.Println("error reading file: ", err)
+			return
+		}
+
+		// Concatenate, split by newline
+		currentLine += string(buffer[:bytesRead])
+		parts := strings.Split(currentLine, "\n")
+		// Print all lines except last
+		for _, part := range parts[:len(parts)-1] {
+			fmt.Println("Line:", part)
+			time.Sleep(time.Second * 1)
+		}
+		// Last part could be incomplete, keep for next round
+		currentLine = parts[len(parts)-1]
+	}
 }
